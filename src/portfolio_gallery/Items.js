2e6cc7822fc42c6c5936a5f55842821d3cd3d727
Nkululeko Rwaxa <46945487+NkululekoRwaxa@users.noreply.github.com>
2023-06-18 23:55:02 +0200

    Update Items.js
    Add alt attribute to img tag

--- src/portfolio_gallery/Items.js
+++ src/portfolio_gallery/Items.js
@@ -7,7 +7,7 @@ function Items({data}){
                     return(
                         <div className="col-sm-3 my-3" key={id}>
                             <div className="card bg-light text-center">
-                                <img src={image} className="img-fluid" style={{height:"200px"}}/>
+                                <img src={image} className="img-fluid" style={{height:"200px"}} alt="Logo"/>
                                 <h3>{title}</h3>
                                 <p>{description}</p>
                             </div>
