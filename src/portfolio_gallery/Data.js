0c01371e113e9c234af8eb2be60c8f58b86d0ac8
NkululekoRwaxa <nkululekorwaxa@gmail.com>
2023-06-18 23:04:08 +0200

    first commit

---
+++ src/portfolio_gallery/Data.js
@@ -0,0 +1,124 @@
+const data =[
+    {
+        id:1,
+        image:"./images/html.png",
+        title:"HTML Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"frontend",
+       
+    },
+    {
+        id:2,
+        image:"./images/css.png",
+        title:"CSS Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"frontend",
+       
+    },
+    {
+        id:3,
+        image:"./images/javascript.png",
+        title:"JS Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"frontend",
+       
+    },
+    {
+        id:4,
+        image:"./images/react.svg",
+        title:"React Js Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"frontend",
+       
+    },
+    {
+        id:5,
+        image:"./images/bootstrap.png",
+        title:"Bootstrap Tutorial",
+        description:"Lorem Ipsum simple dummy ",
+        category:"frontend",
+       
+    },
+    {
+        id:6,
+        image:"./images/php.png",
+        title:"PHP Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"backend",
+       
+    },
+    {
+        id:7,
+        image:"./images/nodejs.png",
+        title:"Node.js Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"backend",
+       
+    },
+    {
+        id:8,
+        image:"./images/asp.jfif",
+        title:"ASP Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"backend",
+       
+    },
+    {
+        id:9,
+        image:"./images/sql.png",
+        title:"SQL Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"database",
+       
+    },
+    {
+        id:10,
+        image:"./images/mongodb.jfif",
+        title:"Mongo DB Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the print... ",
+        category:"database",
+       
+    },
+    {
+        id:11,
+        image:"./images/c.png",
+        title:"C Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"programming",
+       
+    },
+    {
+        id:12,
+        image:"./images/c++.png",
+        title:"C++ Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"programming",
+       
+    },
+    {
+        id:13,
+        image:"./images/java.webp",
+        title:"Java Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"programming",
+       
+    },
+    {
+        id:14,
+        image:"./images/python.jfif",
+        title:"Python Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+        category:"programming",
+       
+    },
+    {
+        id:15,
+        image:"./images/go.png",
+        title:"Go Tutorial",
+        description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+        category:"programming",
+       
+    },
+]
+
+export default data;
